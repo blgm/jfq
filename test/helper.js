c52ca5bd49df2a4fa633ec4709ff6ae731dd502f
George Blue <gblue@pivotal.io>
2018-01-31 17:59:49 +0000

    Refactor the helper

--- test/helper.js
+++ test/helper.js
@@ -2,10 +2,12 @@ import CliTest from 'command-line-test'
 
 export function run (...parms) {
   const cliTest = new CliTest()
-  return cliTest.exec('./bin/jfq.js ' + parms.join(' '))
+  return cliTest.exec(command(parms))
 }
 
 export function runStdin (stdin, ...parms) {
   const cliTest = new CliTest()
-  return cliTest.exec(`echo '${stdin}' | ./bin/jfq.js ` + parms.join(' '))
+  return cliTest.exec(`echo '${stdin}' | ` + command(parms))
 }
+
+const command = parms => './bin/jfq.js ' + parms.join(' ')
