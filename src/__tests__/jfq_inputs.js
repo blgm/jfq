df211db82307cc5b5c49f9b77450d31ec1baf38e
George Blue <gblue@pivotal.io>
2018-02-09 16:32:38 +0000

    Test for error when input JSON is invalid

--- src/__tests__/jfq_inputs.js
+++ src/__tests__/jfq_inputs.js
@@ -1,14 +1,38 @@
 /* eslint-env jest */
 
-import {run} from '../test-helper'
+import {run, runStdin} from '../test-helper'
 
 describe('inputs', () => {
-  it('takes input from a file', () => {
-    return run('$', 'package.json')
+  describe('valid inputs', () => {
+    it('takes input from a file', () => {
+      return run('$', 'package.json')
+        .then(res => {
+          expect(res.error).toBeNull()
+          expect(res.stderr).toBe('')
+          expect(JSON.parse(res.stdout).name).toEqual('jfq')
+        })
+    })
+
+    it('takes input from STDIN', () => {
+      const input = '{"fake":"data"}'
+
+      return runStdin(input, 'fake')
+        .then(res => {
+          expect(res.error).toBeNull()
+          expect(res.stderr).toBe('')
+          expect(res.stdout).toEqual('"data"')
+        })
+    })
+  })
+
+  describe('invalid input', () => {
+    const input = '{"invalid'
+
+    return runStdin(input)
       .then(res => {
-        expect(res.error).toBeNull()
-        expect(res.stderr).toBe('')
-        expect(JSON.parse(res.stdout).name).toEqual('jfq')
+        expect(res.stderr).toBeNull()
+        expect(res.stdout).toBeNull()
+        expect(res.error.message).toContain(`Unexpected token  in JSON at position 9 while parsing '{"invalid'`)
       })
   })
 
