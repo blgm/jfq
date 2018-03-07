16ab3f671e8192a81180c5b0ca59f7c2684142c2
George Blue <blgm@users.noreply.github.com>
2018-03-07 17:51:39 +0000

    Rewrite Promises as async/await (#8)

--- src/__tests__/getopts.js
+++ src/__tests__/getopts.js
@@ -5,33 +5,27 @@ const fakeArgv = (...opts) => ([process.execPath, '/tmp/fakePath.js', ...opts])
 
 describe('getting command line options', () => {
   describe('when there are no options', () => {
-    it('returns default values', () => {
-      return getopts(fakeArgv())
-        .then(res => {
-          expect(res.query).toBe('$')
-          expect(res.files).toEqual([])
-          expect(res.ndjson).toBe(false)
-        })
+    it('returns default values', async () => {
+      const res = await getopts(fakeArgv())
+      expect(res.query).toBe('$')
+      expect(res.files).toEqual([])
+      expect(res.ndjson).toBe(false)
     })
   })
 
   describe('when there is a query and a file', () => {
-    it('reads the query and the file', () => {
-      return getopts(fakeArgv('fake.query', 'fake.file'))
-        .then(res => {
-          expect(res.query).toBe('fake.query')
-          expect(res.files).toEqual(['fake.file'])
-        })
+    it('reads the query and the file', async () => {
+      const res = await getopts(fakeArgv('fake.query', 'fake.file'))
+      expect(res.query).toBe('fake.query')
+      expect(res.files).toEqual(['fake.file'])
     })
   })
 
   describe('when the first argument is a file and not a query', () => {
-    it('reads it as a file, using the default query', () => {
-      return getopts(fakeArgv('package.json'))
-        .then(res => {
-          expect(res.query).toBe('$')
-          expect(res.files).toEqual(['package.json'])
-        })
+    it('reads it as a file, using the default query', async () => {
+      const res = await getopts(fakeArgv('package.json'))
+      expect(res.query).toBe('$')
+      expect(res.files).toEqual(['package.json'])
     })
   })
 })
--- src/__tests__/jfq_inputs.js
+++ src/__tests__/jfq_inputs.js
@@ -4,119 +4,95 @@ import {run, runStdin} from '../test-helper'
 
 describe('inputs', () => {
   describe('methods of input', () => {
-    it('takes input from a file', () => {
-      return run('$', 'package.json')
-        .then(res => {
-          expect(res.error).toBeNull()
-          expect(res.stderr).toBe('')
-          expect(JSON.parse(res.stdout).name).toEqual('jfq')
-        })
+    it('takes input from a file', async () => {
+      const res = await run('$', 'package.json')
+      expect(res.error).toBeNull()
+      expect(res.stderr).toBe('')
+      expect(JSON.parse(res.stdout).name).toEqual('jfq')
     })
 
-    it('takes input from STDIN', () => {
+    it('takes input from STDIN', async () => {
       const input = '{"fake":"data"}'
-
-      return runStdin(input, 'fake')
-        .then(res => {
-          expect(res.error).toBeNull()
-          expect(res.stderr).toBe('')
-          expect(res.stdout).toEqual('data')
-        })
+      const res = await runStdin(input, 'fake')
+      expect(res.error).toBeNull()
+      expect(res.stderr).toBe('')
+      expect(res.stdout).toEqual('data')
     })
 
     describe('when there are multiple files', () => {
-      it('takes input from multiple files', () => {
-        return run('foo', 'src/__tests__/fixtures/a.json', 'src/__tests__/fixtures/b.json')
-          .then(res => {
-            expect(res.error).toBeNull()
-            expect(res.stderr).toBe('')
-            expect(res.stdout).toEqual('bar\nbaz')
-          })
+      it('takes input from multiple files', async () => {
+        const res = await run('foo', 'src/__tests__/fixtures/a.json', 'src/__tests__/fixtures/b.json')
+        expect(res.error).toBeNull()
+        expect(res.stderr).toBe('')
+        expect(res.stdout).toEqual('bar\nbaz')
       })
 
-      it('joins the output of arrays nicely', () => {
-        return run('listy', 'src/__tests__/fixtures/a.json', 'src/__tests__/fixtures/b.json')
-          .then(res => {
-            expect(res.error).toBeNull()
-            expect(res.stderr).toBe('')
-            expect(res.stdout).toEqual('alpha\nbeta\ngamma\ndelta')
-          })
+      it('joins the output of arrays nicely', async () => {
+        const res = await run('listy', 'src/__tests__/fixtures/a.json', 'src/__tests__/fixtures/b.json')
+        expect(res.error).toBeNull()
+        expect(res.stderr).toBe('')
+        expect(res.stdout).toEqual('alpha\nbeta\ngamma\ndelta')
       })
     })
   })
 
   describe('ommitting the expression', () => {
-    it('treats the first argument as an input file', () => {
-      return run('package.json')
-        .then(res => {
-          expect(res.error).toBeNull()
-          expect(res.stderr).toBe('')
-          expect(JSON.parse(res.stdout).name).toEqual('jfq')
-        })
+    it('treats the first argument as an input file', async () => {
+      const res = await run('package.json')
+      expect(res.error).toBeNull()
+      expect(res.stderr).toBe('')
+      expect(JSON.parse(res.stdout).name).toEqual('jfq')
     })
   })
 
   describe('JSON parse errors', () => {
-    it('reports the position in STDIN', () => {
+    it('reports the position in STDIN', async () => {
       const input = '{"invalid'
-
-      return runStdin(input)
-        .then(res => {
-          expect(res.stderr).toBeNull()
-          expect(res.stdout).toBeNull()
-          expect(res.error.message).toContain(`Unexpected token  in JSON at position 9 while parsing '{"invalid'`)
-        })
+      const res = await runStdin(input)
+      expect(res.stderr).toBeNull()
+      expect(res.stdout).toBeNull()
+      expect(res.error.message).toContain(`Unexpected token  in JSON at position 9 while parsing '{"invalid'`)
     })
 
-    it('reports the position and file name for files', () => {
-      return run('$', 'src/__tests__/fixtures/bad.json')
-        .then(res => {
-          expect(res.stderr).toBeNull()
-          expect(res.stdout).toBeNull()
-          expect(res.error.message).toContain(`Unexpected token f in JSON at position 4 while parsing near '{  foo: 42}' in src/__tests__/fixtures/bad.json`)
-        })
+    it('reports the position and file name for files', async () => {
+      const res = await run('$', 'src/__tests__/fixtures/bad.json')
+      expect(res.stderr).toBeNull()
+      expect(res.stdout).toBeNull()
+      expect(res.error.message).toContain(`Unexpected token f in JSON at position 4 while parsing near '{  foo: 42}' in src/__tests__/fixtures/bad.json`)
     })
   })
 
   describe('YAML input', () => {
-    it('accepts YAML input', () => {
-      return run('-a', 'baz', 'src/__tests__/fixtures/c.yaml')
-        .then(res => {
-          expect(res.error).toBeNull()
-          expect(res.stderr).toBe('')
-          expect(res.stdout).toBe('alpha\nbeta')
-        })
+    it('accepts YAML input', async () => {
+      const res = await run('-a', 'baz', 'src/__tests__/fixtures/c.yaml')
+      expect(res.error).toBeNull()
+      expect(res.stderr).toBe('')
+      expect(res.stdout).toBe('alpha\nbeta')
     })
 
     describe('parse errors', () => {
-      it('reports the file name', () => {
-        return run('-a', '$', 'src/__tests__/fixtures/bad.yaml')
-          .then(res => {
-            expect(res.stderr).toBeNull()
-            expect(res.stdout).toBeNull()
-            expect(res.error.message).toContain(`in file src/__tests__/fixtures/bad.yaml`)
-          })
+      it('reports the file name', async () => {
+        const res = await run('-a', '$', 'src/__tests__/fixtures/bad.yaml')
+        expect(res.stderr).toBeNull()
+        expect(res.stdout).toBeNull()
+        expect(res.error.message).toContain(`in file src/__tests__/fixtures/bad.yaml`)
       })
 
-      it('does not report file names with stdin', () => {
-        return runStdin('{foo}}', '-a')
-          .then(res => {
-            expect(res.stderr).toBeNull()
-            expect(res.stdout).toBeNull()
-            expect(res.error.message).toContain(`end of the stream or a document separator is expected at line 1, column 6:\n    {foo}}\n         ^`)
-          })
+      it('does not report file names with stdin', async () => {
+        const res = await runStdin('{foo}}', '-a')
+        expect(res.stderr).toBeNull()
+        expect(res.stdout).toBeNull()
+        expect(res.error.message).toContain(`end of the stream or a document separator is expected at line 1, column 6:\n    {foo}}\n         ^`)
       })
     })
   })
 
   describe('files not found', () => {
-    it('reports the error', () => {
-      return run('$', 'not_here_sucker.json')
-        .then(res => {
-          expect(res.stderr).toBeNull()
-          expect(res.stdout).toBeNull()
-          expect(res.error.message).toContain(`ENOENT: no such file or directory, open 'not_here_sucker.json'`)
-        })
+    it('reports the error', async () => {
+      const res = await run('$', 'not_here_sucker.json')
+      expect(res.stderr).toBeNull()
+      expect(res.stdout).toBeNull()
+      expect(res.error.message).toContain(`ENOENT: no such file or directory, open 'not_here_sucker.json'`)
     })
   })
 })
