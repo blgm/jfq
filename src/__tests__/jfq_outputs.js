ef89762000b78831cd0738eda254a159b5ded0f1
George Blue <oscblgm@gmail.com>
2018-09-20 23:35:59 +0100

    updates for standard@12

--- src/__tests__/jfq_outputs.js
+++ src/__tests__/jfq_outputs.js
@@ -1,6 +1,6 @@
 /* eslint-env jest */
 
-import {run, runStdin} from '../test-helper'
+import { run, runStdin } from '../test-helper'
 import YAML from 'js-yaml'
 
 describe('output format', () => {
@@ -49,7 +49,7 @@ describe('output format', () => {
     })
 
     it('prints as JSON when the -j flag is specified', async () => {
-      const data = {arr: [1, 'foo']}
+      const data = { arr: [1, 'foo'] }
       const input = JSON.stringify(data)
       const expected = JSON.stringify(data.arr, null, 2)
       const res = await runStdin(input, '-j', 'arr')
@@ -61,7 +61,7 @@ describe('output format', () => {
 
   describe('when the output is an array with complex types', () => {
     it('prints JSON', async () => {
-      const data = {arr: [1, 'foo', [6]]}
+      const data = { arr: [1, 'foo', [6]] }
       const input = JSON.stringify(data)
       const expected = JSON.stringify(data.arr, null, 2)
       const res = await runStdin(input, 'arr')
@@ -73,7 +73,7 @@ describe('output format', () => {
 
   describe('default', () => {
     it('outputs as formatted JSON over multiple lines', async () => {
-      const data = {fake: [{json: 'data'}]}
+      const data = { fake: [{ json: 'data' }] }
       const input = JSON.stringify(data)
       const expected = JSON.stringify(data, null, 2)
       const res = await runStdin(input)
@@ -85,7 +85,7 @@ describe('output format', () => {
 
   describe('when the `-n` flag is specified', () => {
     it('outputs as formatted JSON', async () => {
-      const data = {fake: [{json: 'data'}]}
+      const data = { fake: [{ json: 'data' }] }
       const input = JSON.stringify(data)
       const expected = JSON.stringify(data)
       const res = await runStdin(input, '-n')
@@ -106,7 +106,7 @@ describe('output format', () => {
 
   describe('when the `-y` flag is specified', () => {
     it('prints output in YAML', async () => {
-      const data = {fake: [{json: 'data'}]}
+      const data = { fake: [{ json: 'data' }] }
       const input = JSON.stringify(data)
       const expected = YAML.safeDump(data).trim()
       const res = await runStdin(input, '-y')
