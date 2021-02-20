e5065e5937b7166406ce8b3e8a934615d5a7493f
martinb <martinbraun123@aol.com>
2021-02-20 13:41:22 +0100

    introduce support for jsonlines input files

--- src/getopts.js
+++ src/getopts.js
@@ -10,6 +10,7 @@ export default async argv => {
     .option('-a, --accept-yaml', 'YAML input')
     .option('-p, --plain-text', 'Do not decorate output')
     .option('-q, --query-file <path>', 'JSONata query file')
+    .option('-l, --jsonlines', 'JSON Lines Input')
     .parse(argv)
     .opts()
 
@@ -19,10 +20,19 @@ export default async argv => {
   const yamlIn = !!options.acceptYaml
   const plainText = !!options.plainText
   const queryFile = options.queryFile
+  const jsonlines = !!options.jsonlines
   const files = program.args.slice(0)
 
+  if (yamlIn && jsonlines) {
+    throw new Error('--accept-yaml and --jsonlines can not be set together')
+  }
+
+  if (jsonlines && !ndjson) {
+    throw new Error('--jsonlines requires --ndjson (Newline Delimited JSON) as output')
+  }
+
   const query = await getQuery(queryFile, files)
-  return { query, files, ndjson, json, yamlOut, yamlIn, plainText }
+  return { query, files, ndjson, json, yamlOut, yamlIn, plainText, jsonlines }
 }
 
 const exists = async (path) => {
